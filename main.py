c91b85fb4c229451b26bbf4fc7d447f3194ed68a
SucoDeFrutajj <mysterminerbr13@gmail.com>
2026-06-23 08:35:13 -0300

    Update main.py

--- main.py
+++ main.py
@@ -2,8 +2,12 @@ import os
 from cloudlink import cloudlink
 
 if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 3000))
-    # Inicializa o objeto do CloudLink
-    server = cloudlink()
-    # Nas versões 0.1.9.x o comando correto é .server e os parâmetros são nesta ordem
-    server.server(port=port, host="0.0.0.0")
+    # Inicializa o objeto principal do CloudLink
+    cl = cloudlink()
+    
+    # Nas versões 0.1.9.x, as propriedades são injetadas direto no objeto antes de ligar
+    cl.host = "0.0.0.0"
+    cl.port = int(os.environ.get("PORT", 3000))
+    
+    # Inicia o servidor usando as configurações injetadas acima
+    cl.server()
